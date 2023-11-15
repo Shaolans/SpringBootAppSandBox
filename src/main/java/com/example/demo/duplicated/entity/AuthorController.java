eb707baaf17ee06bd660366dedd09010c18bb44b
Ling-Chun SO <32326599+Shaolans@users.noreply.github.com>
2023-11-15 20:10:32 +0100

    Update AuthorController.java

--- src/main/java/com/example/demo/duplicated/entity/AuthorController.java
+++ src/main/java/com/example/demo/duplicated/entity/AuthorController.java
@@ -1,14 +1,9 @@
 package com.example.demo.duplicated.entity;
 
-import com.example.demo.MyEntity;
-import com.example.demo.MyEntityRepository;
-import com.example.demo.qualifiedbean.UsingService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
@@ -27,17 +22,22 @@ public class AuthorController {
 
     @GetMapping("/test2")
     public ResponseEntity test2() {
-        return ResponseEntity.ok(authorRepository.find().size());
+        return ResponseEntity.ok(authorRepository.findLeftJoinFetch().size());
     }
 
     @GetMapping("/test3")
     public ResponseEntity test3() {
-        return ResponseEntity.ok(authorRepository.find());
+        return ResponseEntity.ok(authorRepository.findLeftJoinFetchWithEntityGraph().size());
     }
 
     @GetMapping("/test4")
     public ResponseEntity test4() {
         return ResponseEntity.ok(getSession().createQuery("SELECT a FROM Author a LEFT JOIN FETCH a.books").getResultList().size());
+    }
+
+    @GetMapping("/test5")
+    public ResponseEntity test5() {
+        return ResponseEntity.ok(authorRepository.findAllBy().size());
     }
 
     private Session getSession() {
